b01e43dd283756a098c1a0abfec9b24fde60d01e
Andrei Manu <896868@stud.unive.it>
2025-04-14 20:39:48 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/viaggi_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'una-chiave-segreta-super-sicura'
